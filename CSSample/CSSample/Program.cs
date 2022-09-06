af24c63e84acfd639b98e3ae54e5145ea7991c44
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-06 16:07:58 +0900

    C# first commit

---
+++ CSSample/CSSample/Program.cs
@@ -0,0 +1,11 @@
+﻿class First
+{
+    static string program_ver = "sample #1";
+    static void Main()
+    {
+        System.Console.WriteLine(program_ver);
+        System.Console.Write("name >> ");
+        string? name = System.Console.ReadLine();
+        System.Console.WriteLine($"Welcom {name}");
+    }
+}
