4e15c0c6510b2e92fa2c87f32b462f4429978ffb
joshua <kjkq26993@gaia.eonet.ne.jp>
2022-09-06 16:27:12 +0900

    namespace try

--- CSSample/CSSample/Program.cs
+++ CSSample/CSSample/Program.cs
@@ -1,11 +1,16 @@
-﻿class First
+﻿using System;
+
+namespace FirstNameSpace
 {
-    static string program_ver = "sample #1";
-    static void Main()
+    class First
     {
-        System.Console.WriteLine(program_ver);
-        System.Console.Write("name >> ");
-        string? name = System.Console.ReadLine();
-        System.Console.WriteLine($"Welcom {name}");
+        static string program_ver = "sample #1";
+        static void Main()
+        {
+            System.Console.WriteLine(program_ver);
+            System.Console.Write("name >> ");
+            string? name = System.Console.ReadLine();
+            System.Console.WriteLine($"Welcom {name}");
+        }
     }
 }
